0218c563b554e182ccaf0b9b60ae41eae3f92be3
Aditya Verma <aditya13.mitmpl2022@learner.manipal.edu>
2023-10-27 00:44:33 +0530

    added custom cursor, fixed minor issue in icon tracking

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
-//TODO add custom cursor
 //TODO mobile screen
 
 'use strict';
 
+const container = document.querySelector(".container");
 const cover = document.querySelector(".cover");
 const icon = document.querySelector(".icon");
+const cursor = document.querySelector(".cursor");
 const audio = new Audio("audio/straight.mp3");
+
 let randomX;
 let randomY;
 let vol;
 let maxDistance = 1200;
-let isPlaying = false;
 
 icon.addEventListener("mousedown", function () {
-    // audio.remove();
+
     cover.style.opacity = "0";
-    icon.style.opacity = "0";
-    // cover.style.zIndex = "-1";
-    audio.volume = 0.5;
-        
+    icon.style.display = "none";
+    setTimeout(() => {
+        cover.style.display = "none";
+        audio.volume = 0.4;
+    }, 1000);
 });
 
 
-const getColorBasedOnDistance = function(distance, maxDistance) {
+const getColorBasedOnDistance = function (distance, maxDistance) {
     let scale = 255 / maxDistance;
 
     let endColor = [98, 55, 160]; //the purplish colour
@@ -39,22 +41,20 @@ const getColorBasedOnDistance = function(distance, maxDistance) {
 
 const checkForVolume = function (distance) {
     if (distance >= 0 && distance < 50)
-    return 1;
+        return 1;
     else if (distance >= 60 && distance < 480)
-    return 0.5;
+        return 0.67;
     else if (distance >= 480 && distance < 960)
-    return 0.3;
-    else if (distance >= 960 && distance < 1200)
-    return 0.1;
+        return 0.34;
     else
-    return 0;
+        return 0.1;
 }
 
 const randomizePosition = function () {
-    
+
     randomX = Math.floor(Math.random() * (window.innerWidth - icon.clientWidth));
     randomY = Math.floor(Math.random() * (window.innerHeight - icon.clientHeight));
-    
+
     icon.style.left = randomX + 'px';
     icon.style.top = randomY + 'px';
 }
@@ -62,43 +62,22 @@ randomizePosition();
 
 cover.addEventListener("mousemove", function (e) {
     //this function finds distance of icon from the cursor position and sets volume of audio depending on the distance
-    
-    let left = parseInt(icon.style.left);
-    let top = parseInt(icon.style.top);
-    
+
+    const left = parseInt(icon.style.left);
+    const top = parseInt(icon.style.top);
+  
     const distance = Math.sqrt(Math.pow((e.clientX - left), 2) + Math.pow((e.clientY - top), 2));
-    
     vol = checkForVolume(distance);
     console.log(distance);
     audio.volume = vol;
     audio.play();
 
-    let color = getColorBasedOnDistance(distance, maxDistance);
+    const color = getColorBasedOnDistance(distance, maxDistance);
     cover.style.backgroundColor = color;
     console.log(color);
 });
 
-
-/*
-
-
-cover.addEventListener("mousemove", function (e) {
-    //this function finds distance of icon from the cursor position and sets volume of audio depending on the distance
-
-    audio = new Audio("audio/tiktok_haaa_meme.mp3");
-    let left = parseInt(icon.style.left);
-    let top = parseInt(icon.style.top);
-
-    let distance = Math.sqrt(Math.pow((e.x - left), 2) + Math.pow((e.y - top), 2));
-    // console.log(distance);
-
-    let color = getColorBasedOnDistance(distance, maxDistance);
-    cover.style.backgroundColor = color;
-    console.log(color);
-
-});
-
-
-
-
-*/
+container.addEventListener("mousemove", function(e){
+    cursor.style.top = e.y + "px";
+    cursor.style.left = e.x + "px";
+})
