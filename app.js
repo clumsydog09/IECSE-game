6c2efb3880397379aa019f5c7c21b8cc06737bef
Aditya Verma <aditya13.mitmpl2022@learner.manipal.edu>
2023-10-26 16:11:17 +0530

    first commit

--- app.js
+++ app.js
@@ -36,11 +36,13 @@ function randomizePosition() {
 randomizePosition();
 
 cover.addEventListener("mousemove", function (e) {
+    //this function finds distance of icon from the cursor position and sets volume of audio depending on the distance
 
     let left = parseInt(icon.style.left);
     let top = parseInt(icon.style.top);
 
     const distance = Math.sqrt(Math.pow((e.clientX - left), 2) + Math.pow((e.clientY - top), 2));
+    
     let vol = checkForVolume(distance);
     console.log(distance);
     audio.volume = vol;
